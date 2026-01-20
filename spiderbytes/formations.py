fa27c624b304d842e8f4471aaa31715afc6e6c09
pavansai018 <18pavansai@gmail.com>
2026-01-21 00:17:50 +0800

    added variables, formations

--- spiderbytes/formations.py
+++ spiderbytes/formations.py
@@ -16,23 +16,88 @@ class HomePosition(Node):
         )
 
         self.joint_names = variables.dog_joints
+        self.rotation_sign = variables.dog_joints_rotation_signs
+        self.target_position = [
+                variables.spider_home_angle_from_dog_pose['hip'],variables.spider_home_angle_from_dog_pose['knee'], variables.spider_home_angle_from_dog_pose['leg'],
+                variables.spider_home_angle_from_dog_pose['hip'],variables.spider_home_angle_from_dog_pose['knee'], variables.spider_home_angle_from_dog_pose['leg'],
+                variables.spider_home_angle_from_dog_pose['hip'],variables.spider_home_angle_from_dog_pose['knee'], variables.spider_home_angle_from_dog_pose['leg'],
+                variables.spider_home_angle_from_dog_pose['hip'],variables.spider_home_angle_from_dog_pose['knee'], variables.spider_home_angle_from_dog_pose['leg'],
+            ]
+        self.current_position = [0.0] * 12
+    def send_pose(self, positions, duration=0.5):
+        msg = JointTrajectory()
+        msg.joint_names = self.joint_names
 
+        pt = JointTrajectoryPoint()
+        pt.positions = positions
+        pt.time_from_start.sec = int(duration)
+        pt.time_from_start.nanosec = int((duration % 1) * 1e9)
+
+        msg.points = [pt]
+        self.pub.publish(msg)
+        time.sleep(duration)
+    def spider_pose(self):
+        for group in variables.joint_groups_spider:
+            msg = JointTrajectory()
+            msg.joint_names = self.joint_names
+            pt = JointTrajectoryPoint()
+            pt.positions = self.current_position
+            for idx in group:
+                joint = self.joint_names[idx]
+                pt.positions[idx] = (
+                    self.rotation_sign[joint]
+                    * variables.spider_pose_joint_signs[joint]
+                    * self.target_position[idx]
+                )
+            pt.time_from_start.sec = 1
+            msg.points = [pt]
+            self.pub.publish(msg)
+            # print(f'Group: {group}, PrePos: {pt.positions}')
+            time.sleep(1.1)
+            pt.positions[group[1]] = 0
+            msg.points = [pt]
+            self.pub.publish(msg)
+            time.sleep(1.1)
+            self.current_position = pt.positions
+            # print(f'Group: {group}, PostPos: {pt.positions}')
+
+        for key, value in variables.knee_joints.items():
+            self.current_position[key] = self.rotation_sign[value] * variables.spider_pose_joint_signs[value] * variables.spider_crouch_position['knee']
+        pt.positions = self.current_position
+        pt.time_from_start.sec = 1
+        msg.points = [pt]
+        self.pub.publish(msg)
+
+    def home_position(self):
+        for group in variables.joint_groups_dog:
+            msg = JointTrajectory()
+            msg.joint_names = self.joint_names
+            pt = JointTrajectoryPoint()
+            pt.positions = self.current_position
+            for idx in group:
+                joint = self.joint_names[idx]
+                pt.positions[idx] = (
+                    self.rotation_sign[joint]
+                    * variables.spider_pose_joint_signs[joint]
+                    * 0
+                )
+            pt.time_from_start.sec = 1
+            msg.points = [pt]
+            self.pub.publish(msg)
+            time.sleep(1.1)
+            pt.positions[group[1]] = 0
+            msg.points = [pt]
+            self.pub.publish(msg)
+            time.sleep(1.1)
+            self.current_position = pt.positions
     def publish_once(self):
         # Optional: Wait for a subscriber to connect so the message isn't lost
         while self.pub.get_subscription_count() == 0:
             self.get_logger().info('Waiting for subscriber...')
             time.sleep(0.5)
-
-        msg = JointTrajectory()
-        msg.joint_names = self.joint_names
-        
-        pt = JointTrajectoryPoint()
-        pt.positions = [0.0] * 12  # Simplified 12-joint list
-        pt.time_from_start.sec = 1 # Recommended: tell controller how long to take
-        
-        msg.points = [pt]
-        
-        self.pub.publish(msg)
+        self.spider_pose()
+        time.sleep(5)
+        self.home_position()
         self.get_logger().info("Home Position Sent. Shutting down...")
 
 def main():
--- spiderbytes/variables.py
+++ spiderbytes/variables.py
@@ -1,3 +1,4 @@
+
 dog_joints = [
     "hip_joint_fl","knee_joint_fl","leg_joint_fl",
     "hip_joint_fr","knee_joint_fr","leg_joint_fr",
@@ -11,3 +12,82 @@ spiderbytes_joints = [
     "joint_rotation_3","joint_limb_3","joint_leg_3",
     "joint_rotation_4","joint_limb_4","joint_leg_4",
 ]
+# in dog robot, the rotation sign of each joint. for example -1 * radian on hip_fl makes it move forward otherwise moves backward
+dog_joints_rotation_signs = {
+    'hip_joint_fl': -1,
+    'knee_joint_fl': -1,
+    'leg_joint_fl': -1,
+    'hip_joint_fr': +1,
+    'knee_joint_fr': -1,
+    'leg_joint_fr': -1,
+    'hip_joint_rl': -1,
+    'knee_joint_rl': -1,
+    'leg_joint_rl': -1,
+    'hip_joint_rr': +1,
+    'knee_joint_rr': -1,
+    'leg_joint_rr': -1,
+}
+
+# follow these signs on dog robot to convert it into spider pose. should apply these signs on top of above dog_joints_rotation_signs
+spider_pose_joint_signs = {
+    'hip_joint_fl': +1,
+    'knee_joint_fl': +1,
+    'leg_joint_fl': +1,
+    'hip_joint_fr': +1,
+    'knee_joint_fr': +1,
+    'leg_joint_fr': +1,
+    'hip_joint_rl': -1,
+    'knee_joint_rl': +1,
+    'leg_joint_rl': +1,
+    'hip_joint_rr': -1,
+    'knee_joint_rr': +1,
+    'leg_joint_rr': +1,
+}
+
+
+
+spider_home_angle_from_dog_pose = {
+        'hip': (3.14 / 2) / 2,
+        'knee': 0.3, #(3.14 / 2) / 3,
+        'leg': 0.0
+    }
+spider_crouch_position = {
+    'hip': spider_home_angle_from_dog_pose['hip'],
+    'knee': (3.14 / 2) / 4,
+    'leg': spider_home_angle_from_dog_pose['leg']
+}
+
+joint_groups_spider = [
+    
+    [0, 1, 2],
+    [9, 10, 11],
+    [3, 4, 5],
+    [6, 7, 8],
+]
+joint_groups_dog = [
+    
+    [0, 1, 2],
+    
+    [3, 4, 5],
+    [6, 7, 8],
+    [9, 10, 11],
+]
+hip_joints = {
+    0: 'hip_joint_fl',
+    3: 'hip_joint_fr',
+    6: 'hip_joint_rl',
+    9: 'hip_joint_rr',
+}
+
+knee_joints = {
+    1: 'knee_joint_fl',
+    4: 'knee_joint_fr',
+    7: 'knee_joint_rl',
+    10: 'knee_joint_rr',
+}
+leg_joints = {
+    2: 'leg_joint_fl',
+    5: 'leg_joint_fr',
+    8: 'leg_joint_rl',
+    11: 'leg_joint_rr',
+}
